a6f2ace943e313d35baf76fc8f98fa3434daf453
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-06-16 10:11:47 +0000

    change debug in setting to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)a)cspq0=(@9$(m_4v0+btzc$n8#avjb^1iclz%2nj5s)f=6o9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rosencrantz-coolcations-0urbd4yrnbq.ws-eu114.gitpod.io','.herokuapp.com']
 
